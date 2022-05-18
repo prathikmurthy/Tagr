6684fcda1b7859bc7e94ad06c42bd4d4a9d97e46
Prathik M <murthy.prathik@gmail.com>
2022-05-18 00:08:23 -0400

    init

---
+++ src/src/components/header.js
@@ -0,0 +1,10 @@
+import {react, Component} from 'react';
+
+export default function Header() {
+    return (
+        <>
+            <h1 class="text-white text-5xl text-left pt-20 pl-20">IMO Data Tagging</h1>
+            <p class="text-gray-600 text-3xl text-left pl-20 pt-10">Tag images on the right using the icons below</p>
+        </>
+    )
+}
