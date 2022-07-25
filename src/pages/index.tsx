65764da7543b7e18466cb0bb9ace064f0de6bce7
Prathik M <murthy.prathik@gmail.com>
2022-07-25 17:03:12 -0400

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import ImageComponent from '../components/ImageComponent'
 import styles from '../styles/Home.module.css'
-import ImageFactory from '../components/ImageFactory'
 import { useState } from 'react'
 import Main from './main'
 
